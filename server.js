8ec5c2da67354ec7c817155a7947b1d3e5a7e84d
Dayton-IX <daytonmaximus@gmail.com>
2022-05-20 13:53:22 -0600

    request params

--- server.js
+++ server.js
@@ -12,10 +12,22 @@ const init = async () => {
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-
             return '<h1>Hello World!</h1>';
         }
     });
+
+    server.route({
+        method: 'GET',
+        path: '/users/{user?}',
+        handler: (request, h) => {
+
+            if (request.params.user) {
+                return `<h1>Hello ${request.params.user}!</h1>`;
+            } else {
+                return `<h1>Hello Stranger!</h1>`;
+            }
+        }
+    })
 
     await server.start();
     console.log('Server running on: ', server.info.uri);

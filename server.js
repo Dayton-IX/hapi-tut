d327324ed6595c44019dad6bde0f87a145618796
Dayton-IX <daytonmaximus@gmail.com>
2022-05-20 11:51:23 -0600

    hello world on root route

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = hapi.server({
+        port: 1234,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return '<h1>Hello World!</h1>';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on: ', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.error(err);
+    process.exit(1);
+})
+
+init();
